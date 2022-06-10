ef7bb55d7af84d00e66b1147b0919c27f3320d2e
HedianTea <daki9981@qq.com>
2022-06-10 22:31:18 +0800

    自动优化包结构

--- javaBasics/src/main/java/com/hediancha/array/ArrayTest.java
+++ javaBasics/src/main/java/com/hediancha/array/ArrayTest.java
@@ -2,12 +2,10 @@ package com.hediancha.array;
 
 import com.hediancha.bean.UserDto;
 import com.hediancha.bean.UserInfoDto;
-import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
